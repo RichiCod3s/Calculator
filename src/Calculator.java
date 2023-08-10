05d72d53df4231c03c928ce086dd370ddfc35493
RichiCod3s <your@email.com>
2023-08-10 16:28:46 +0100

    Buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,11 @@ public class Calculator implements ActionListener  {
 
 	JFrame frame;
 	JTextField textfield;
+	
+	//array of number buttons
 	JButton[] numberButtons = new JButton[10];
+	
+	// array of function buttons - *, -, Delete etc
 	JButton[] functionButtons = new JButton[8];
 	JButton addButton,subButton, mulButton, divButton;
 	JButton decButton, equButton, delButton, clrButton;
@@ -20,6 +24,48 @@ public class Calculator implements ActionListener  {
 	
 	Calculator(){
 		
+		// frame
+		frame = new JFrame("Calculator"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,550);
+		frame.setLayout(null);
+		
+		//textfield
+		textfield = new JTextField();
+		textfield.setBounds(50, 25, 300, 50); // x coordinate, y cord, width, height
+		textfield.setFont(myFont);
+		textfield.setEditable(false); // this ensures user cannot edit the textfield top of JFrame (Calculator);
+		
+		//buttons for frame
+		addButton = new JButton("+");
+		subButton = new JButton("-");
+		mulButton = new JButton("*");
+		divButton = new JButton("/");
+		decButton = new JButton(".");
+		equButton = new JButton("=");
+		delButton = new JButton("Delete");
+		clrButton = new JButton("Clear");
+		
+		// add buttons to array 
+		functionButtons[0] = addButton;
+		functionButtons[1] = subButton;
+		functionButtons[2] = mulButton;
+		functionButtons[3] = divButton;
+		functionButtons[4] = decButton;
+		functionButtons[5] = equButton;
+		functionButtons[6] = delButton;
+		functionButtons[7] = clrButton;
+		
+		
+		//loop through buttons to do a few things
+		for(int i=0; i<8; i++) {
+			functionButtons[i].addActionListener(this);
+			functionButtons[i].setFont(myFont);
+			functionButtons[i].setFocusable(false); // gets rid of outline around the buttons - esthetic
+		}
+		
+		frame.add(textfield); // add textfield to JFrame
+		frame.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
@@ -28,4 +74,10 @@ public class Calculator implements ActionListener  {
 	
 	
 }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
